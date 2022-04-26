b03256210a2d966754789f879757750c822da565
joanna <okokis101@gmail.com>
2022-04-27 01:48:21 +0800

    完成基本 Coffee CRUD

--- src/main/java/com/example/testDemo/TestDemoApplication.java
+++ src/main/java/com/example/testDemo/TestDemoApplication.java
@@ -2,6 +2,9 @@ package com.example.testDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @SpringBootApplication
 public class TestDemoApplication {
@@ -10,4 +13,110 @@ public class TestDemoApplication {
 		SpringApplication.run(TestDemoApplication.class, args);
 	}
 
+}
+
+@RestController
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(
+				List.of(
+						new Coffee("Caf'e Cereza"),
+						new Coffee("Caf'e Ganador"),
+						new Coffee("Caf'e Lareno"),
+						new Coffee("Caf'e Tres Pontas")
+				)
+		);
+		coffees.add(new Coffee("Caf'e Brown"));
+	}
+
+	// 得到全部的咖啡資料
+	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
+//	@GetMapping("/coffees")
+	Iterable<Coffee> getCoffees() {
+		return coffees;
+	}
+
+	// 得到某一筆的咖啡資料，傳入 id 查詢
+	@GetMapping("/coffees/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				return Optional.of(c);
+			}
+		}
+		return Optional.empty();
+	}
+
+//	@GetMapping("/coffees/{id}")
+//	List<Coffee> getCoffeeById(@PathVariable String id) {
+//		for (Coffee c: coffees) {
+//			if (c.getId().equals(id)) {
+//				return List.of(c);
+//			}
+//		}
+//		return Collections.emptyList();
+//	}
+
+
+	@PostMapping("/coffees")
+	List<Coffee> addCoffee(@RequestBody Coffee coffee) {
+		coffees.add(new Coffee(coffee.getName()));
+		return coffees;
+	}
+	@PostMapping("/coffees2")
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+//		coffees.add(coffee); => 造成 id 為 null
+		coffees.add(new Coffee(coffee.getName()));
+		return coffee;
+	}
+
+	// 若無此更新資料就新增一筆，若有此資料就更新
+	@PutMapping("/coffees/{id}")
+	Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+		int coffeeIndex = -1;
+
+		for (Coffee c: coffees) {
+			if (c.getId().equals(id)) {
+				coffeeIndex = coffees.indexOf(c);
+				coffees.set(coffeeIndex, coffee);
+			}
+		}
+
+		return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+	}
+
+	@DeleteMapping("/coffees/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffees.removeIf(c -> c.getId().equals(id));
+	}
+}
+
+
+class Coffee {
+	// 設為 final 只能被指派一次，且不能被修改
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
